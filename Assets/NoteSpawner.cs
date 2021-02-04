84c8a2f40ebc141150fa4cc9249075d8590c54e4
Jarryl Oquias <jaroqui17@gmail.com>
2021-02-03 19:29:24 -0800

    Moved to github

---
+++ Assets/NoteSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawner : MonoBehaviour
+{
+    public Transform up;
+    public Transform down;
+    public Transform left;
+    public Transform right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnNotes());
+    }
+
+    IEnumerator spawnNotes() {
+        yield return new WaitForSeconds(1);
+        Instantiate(up, transform.GetChild(3));
+
+        yield return new WaitForSeconds(1);
+        Instantiate(up, transform.GetChild(3));
+
+        yield return new WaitForSeconds(1);
+        Instantiate(up, transform.GetChild(3));
+    }
+}
---
+++ Assets/UpInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpInput : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (collision.gameObject.CompareTag("Perfect")
+                && collision.gameObject.CompareTag("Good")
+                && collision.gameObject.CompareTag("Bad"))
+            {
+                GameManager.score += 300;
+                GameManager.health += 5;
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.CompareTag("Good")
+                && collision.gameObject.CompareTag("Bad")) {
+                GameManager.score += 150;
+                GameManager.health += 3;
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.CompareTag("Bad")) {
+                GameManager.score += 50;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
